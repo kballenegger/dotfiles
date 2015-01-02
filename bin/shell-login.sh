b8dfdfe12fa77e09e5299ad9eca8c9a2836e3a16
Kenneth Ballenegger <kenneth@ballenegger.com>
2015-01-02 13:40:48 -0800

    use $SHELL in shell login script

--- bin/shell-login.sh
+++ bin/shell-login.sh
@@ -1,11 +1,20 @@
-type reattach-to-user-namespace &>/dev/null
-if [ $? -eq 0 ]; then
-    reattach-to-user-namespace -l zsh
-else
-    type zsh &>/dev/null
+if [ -x "$SHELL" ]; then
+    type reattach-to-user-namespace &>/dev/null
     if [ $? -eq 0 ]; then
-        zsh -l
+        reattach-to-user-namespace -l "$SHELL"
     else
-        bash -l
+        "$SHELL" -l
+    fi
+else
+    type reattach-to-user-namespace &>/dev/null
+    if [ $? -eq 0 ]; then
+        reattach-to-user-namespace -l zsh
+    else
+        type zsh &>/dev/null
+        if [ $? -eq 0 ]; then
+            zsh -l
+        else
+            bash -l
+        fi
     fi
 fi
