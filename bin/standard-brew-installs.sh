5039d8f9a124db419fdfb30fdd686ffca845df2f
Kenneth Ballenegger <kenneth@ballenegger.com>
2017-01-16 12:47:56 -0800

    std brew installs

---
+++ bin/standard-brew-installs.sh
@@ -0,0 +1,36 @@
+# brew installs
+
+brew install zsh
+brew install tmux
+brew install fasd
+brew install ruby
+brew install python
+brew install httpie
+brew install jq
+brew install git
+brew install ack
+
+brew tap neovim/neovim
+brew install neovim
+
+# just guessing
+
+brew cask install 1password
+brew cask install google-chrome
+brew cask install alfred
+brew cask install divvy
+brew cask install dropbox
+brew cask install daisydisk
+brew cask install iterm2
+brew cask install keybase
+brew cask install sketch
+brew cask install spotify
+brew cask install slack
+brew cask install transmission
+brew cask install airfoil
+brew cask install beamer
+brew cask install bartender
+brew cask install cloudapp
+brew cask install fantastical
+brew cask install pdf-expert
+brew cask install the-unarchiver
