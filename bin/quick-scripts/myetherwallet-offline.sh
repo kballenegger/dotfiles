2a89dd0f42a04c7e821f36ad49a591c561c2b4f4
Kenneth Ballenegger <kenneth@ballenegger.com>
2021-01-28 02:35:16 +0800

    quickscript updates

--- bin/quick-scripts/myetherwallet-offline.sh
+++ bin/quick-scripts/myetherwallet-offline.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-open -a "Safari Offline" ~/Applications/etherwallet-v3.21.12/index.html
+open -a "Safari Offline" ~/Applications/etherwallet/current/index.html
---
+++ bin/quick-scripts/refresh-unlocator.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl -sLI "http://unlo.it/5790d13c726372e"
